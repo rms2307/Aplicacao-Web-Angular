9194fea1c2fbfbe9191b5c0c003f4e7f1cfcc7fa
Robson Silva <rms2307@outlook.com>
2020-08-22 21:12:34 -0300

    ajustes

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+app.use(express.static(__dirname + '/dist/tripletech'));
 
-app.get('/*', function (req, res) {
-
-    res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/tripletech/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
